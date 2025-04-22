9467edf1e789d63b86b67fb977a0340f586e2688
ganesh.balasaheb <ganesh.balasaheb@maqta.ae>
2025-04-22 11:29:36 +0400

    Excel

--- src/components/Excel/Excel.js
+++ src/components/Excel/Excel.js
@@ -1,6 +1,81 @@
+import myData from './data.json';
+import { useEffect, useState } from 'react';
+import "./Excel.css"
+import { saveAs } from 'file-saver';
 
 const ExcelFile = () => {
-    return <>ExcelFile</>
+    const [data, setData] = useState(myData);
+    const [formData, setFormData] = useState({
+        name: '',
+        date: ''
+    });
+
+    useEffect(() => {
+        fetch('./data.json') // Path to JSON file
+            .then(data => {
+                console.log(data)
+                //setData(data);
+            }
+            )
+            .catch(error => console.error('Error loading JSON:', error));
+    }, []);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const data = { name: formData.name, CheckDate: formData.date };
+        myData.Stoks.push(data);
+        const blob = new Blob([JSON.stringify(myData, null, 2)], { type: 'application/json' });
+        saveAs(blob, './data.json');
+        console.log('Form Data:', formData);
+    };
+    return <>
+        <table className='QTable'>
+            <th><tr><td colspan={4}>FinancialQuarter</td></tr></th>
+            <tbody>
+                {data.FinancialQuarter.map((dt, index) => {
+                    return <tr><td key={index}>{dt}</td></tr>
+                })}
+            </tbody>
+        </table>
+        <hr></hr>   <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name:</label>
+                <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>Date:</label>
+                <input
+                    type="date"
+                    name="date"
+                    value={formData.date}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+        <table className='QTable'>
+            <th><tr><td colspan={4}>StockData</td></tr></th>
+            <tr><td>Name</td><td>Date</td></tr>
+            <tbody>
+                {data.Stoks.map((dt, index) => {
+                    return <tr key={index}><td >{dt.Name}</td><td >{dt.CheckDate}</td></tr>
+                })}
+            </tbody>
+        </table>
+    </>
 }
 
 export default ExcelFile;
